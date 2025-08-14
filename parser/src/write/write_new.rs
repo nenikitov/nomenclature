94a38785502134e482099f9060b64fce4ba9c372
nenikitov <nenikitov@mail.ru>
2025-08-13 22:15:41 -0400

    wip

--- parser/src/write/write_new.rs
+++ parser/src/write/write_new.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{Seek, Write},
-    ops::Deref,
+    marker::PhantomData,
 };
 
 use crate::prelude::Endian;
@@ -9,10 +9,10 @@ trait BinWrite<Writer, Args, In>
 where
     Writer: Write + Seek,
 {
-    fn write_non_back_tracking(
+    fn write_non_backtracking(
         &mut self,
-        writer: &mut Writer,
         value: &In,
+        writer: &mut Writer,
         endian: Endian,
         args: Args,
     ) -> Result<(), ()>;
@@ -20,17 +20,17 @@ where
 
 impl<F, Writer, Args, In> BinWrite<Writer, Args, In> for F
 where
-    F: FnMut(&mut Writer, &In, Endian, Args) -> Result<(), ()>,
+    F: FnMut(&In, &mut Writer, Endian, Args) -> Result<(), ()>,
     Writer: Write + Seek,
 {
-    fn write_non_back_tracking(
+    fn write_non_backtracking(
         &mut self,
-        writer: &mut Writer,
         value: &In,
+        writer: &mut Writer,
         endian: Endian,
         args: Args,
     ) -> Result<(), ()> {
-        self(writer, value, endian, args)
+        self(value, writer, endian, args)
     }
 }
 
@@ -43,84 +43,6 @@ trait BinWriter {
         Writer: Write + Seek;
 }
 
-struct BinWriteProxy<T>(T);
-
-trait Test
-where
-    Self: Sized,
-{
-    fn writer(&self) -> BinWriteProxy<&Self> {
-        BinWriteProxy(&self)
-    }
-}
-
-impl<T> BinWriter for BinWriteProxy<T>
-where
-    T: BinWriter,
-{
-    type Args<'a> = T::Args<'a>;
-    type In = T::In;
-
-    fn writer<'a, Writer>() -> impl BinWrite<Writer, Self::Args<'a>, Self::In>
-    where
-        Writer: Write + Seek,
-    {
-        |writer: &mut Writer, value: &Self::In, endian, args| {
-            T::writer().write_non_back_tracking(writer, value, endian, args)
-        }
-    }
-}
-
-impl<T> BinWriteProxy<T> {
-    fn write<'a, 'i, Writer>(
-        &self,
-        writer: &mut Writer,
-        endian: Endian,
-        args: T::Args<'a>,
-    ) -> Result<(), ()>
-    where
-        T: BinWriter<In = T>,
-        Writer: Write + Seek,
-    {
-        T::writer().write_non_back_tracking(writer, &self.0, endian, args)
-    }
-
-    fn map<'a, MapFn, T2>(self, map: MapFn) -> BinWriteProxy<T2>
-    where
-        MapFn: Fn(T) -> T2,
-    {
-        BinWriteProxy(map(self.0))
-    }
-
-    fn repeat(self, times: usize) -> BinWriteProxy<T> {
-        BinWriteProxy(self.0)
-    }
-}
-
-struct Repeat<F>(F, usize);
-
-impl<F, Writer, Args, In> BinWrite<Writer, Args, In> for Repeat<F>
-where
-    Writer: Write + Seek,
-    Args: Clone,
-    F: BinWrite<Writer, Args, In>,
-{
-    fn write_non_back_tracking(
-        &mut self,
-        writer: &mut Writer,
-        value: &In,
-        endian: Endian,
-        args: Args,
-    ) -> Result<(), ()> {
-        for _ in 0..self.1 {
-            self.0
-                .write_non_back_tracking(writer, value, endian, args.clone())?;
-        }
-
-        Ok(())
-    }
-}
-
 impl BinWriter for u8 {
     type Args<'a> = ();
     type In = Self;
@@ -129,16 +51,138 @@ impl BinWriter for u8 {
     where
         Writer: Write + Seek,
     {
-        |writer: &mut Writer, value: &Self::In, endian, args| todo!()
+        |value: &Self::In, writer: &mut Writer, endian, args| {
+            writer.write_all(&value.to_le_bytes()).unwrap();
+            Ok(())
+        }
     }
 }
 
-impl<T> Test for T {}
+trait BinWriterStart {
+    fn writer(&self) -> impl PossiblyBinWriteTrait<&Self> {
+        PossiblyBinWrite(self)
+    }
+}
+
+struct PossiblyBinWrite<T>(T);
+
+trait PossiblyBinWriteTrait<In> {}
+
+impl<In> PossiblyBinWriteTrait<In> for PossiblyBinWrite<In> {}
+
+trait PossiblyBinWriteTraitExt<In>
+where
+    Self: PossiblyBinWriteTrait<In> + Sized,
+{
+    fn write<'a, Writer>(
+        self,
+        writer: &mut Writer,
+        endian: Endian,
+        args: In::Args<'a>,
+    ) -> Result<(), ()>
+    where
+        In: BinWriter<In = In>,
+        Writer: Write + Seek,
+    {
+        In::writer().write_non_backtracking(&self, writer, endian, args)
+    }
+
+    fn map<In2, MapFn>(self, map: MapFn) -> impl PossiblyBinWriteTrait<In2>
+    where
+        MapFn: Fn(&In) -> In2,
+    {
+        Map {
+            inner: self,
+            map,
+            _in: PhantomData,
+        }
+    }
+
+    fn assert<AssertFn>(self, assertion: AssertFn) -> impl PossiblyBinWriteTrait<In>
+    where
+        AssertFn: Fn(&In) -> bool,
+    {
+        Assert {
+            inner: self,
+            assertion,
+        }
+    }
+}
+
+impl<T, In> PossiblyBinWriteTraitExt<In> for T where T: PossiblyBinWriteTrait<In> {}
+
+struct Map<In, Inner, MapFn> {
+    inner: Inner,
+    map: MapFn,
+    _in: PhantomData<In>,
+}
+
+impl<In, In2, Inner, MapFn> PossiblyBinWriteTrait<In2> for Map<In, Inner, MapFn>
+where
+    Inner: PossiblyBinWriteTrait<In>,
+    MapFn: Fn(&In) -> In2,
+{
+}
+
+impl<Inner, MapFn, Writer, Args, In, In2> BinWrite<Writer, Args, In> for Map<In, Inner, MapFn>
+where
+    Writer: Write + Seek,
+    Inner: BinWrite<Writer, Args, In2>,
+    MapFn: Fn(&In) -> In2,
+{
+    fn write_non_backtracking(
+        &mut self,
+        value: &In,
+        writer: &mut Writer,
+        endian: Endian,
+        args: Args,
+    ) -> Result<(), ()> {
+        self.inner
+            .write_non_backtracking(&(self.map)(value), writer, endian, args)
+    }
+}
+
+struct Assert<Inner, AssertFn> {
+    inner: Inner,
+    assertion: AssertFn,
+}
+
+impl<In, Inner, AssertFn> PossiblyBinWriteTrait<In> for Assert<Inner, AssertFn> where
+    Inner: PossiblyBinWriteTrait<In>
+{
+}
+
+impl<Inner, AssertFn, Writer, Args, In> BinWrite<Writer, Args, In> for Assert<Inner, AssertFn>
+where
+    Writer: Write + Seek,
+    Inner: BinWrite<Writer, Args, In>,
+    AssertFn: Fn(&In) -> bool,
+{
+    fn write_non_backtracking(
+        &mut self,
+        value: &In,
+        writer: &mut Writer,
+        endian: Endian,
+        args: Args,
+    ) -> Result<(), ()> {
+        if !(self.assertion)(value) {
+            Err(())
+        } else {
+            self.inner
+                .write_non_backtracking(value, writer, endian, args)
+        }
+    }
+}
+
+impl<T> BinWriterStart for T {}
 
 fn test() {
     let mut buf = std::io::Cursor::new(vec![10u8]);
-    _ = 10u16
+    _ = 10u8
         .writer()
+        .map(|v| **v as usize)
+        .map(|v| *v as u32)
+        .assert(|a| true)
         .map(|v| *v as u8)
-        .write(&mut buf, Endian::Big, ());
+        .write(&mut buf, Endian::Little, ());
 }
